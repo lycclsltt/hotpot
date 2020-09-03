3e02db34d01f4eb969b5ccd3ab75c29827a5e397
liyuchen <liyuchen@changba.com>
2020-09-03 19:06:21 +0800

    modify restart.sh

--- prod_restart.sh
+++ prod_restart.sh
@@ -2,4 +2,7 @@
 rm -f ./settings.ini
 ln -s ./settings.ini.prod ./settings.ini
 ps -ef | grep hotpot | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
-python3 hotpot.py
+mkdir -p logs
+nohup python3 hotpot.py >> ./logs/stdout.log 2>&1 &
+sleep 0.5
+tail -n 20 ./logs/stdout.log
